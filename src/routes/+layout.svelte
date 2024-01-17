59d05eda46c5d4ea4bfd28e54d630decc521c68c
Michael Zhao <michaelzhao314@gmail.com>
2024-01-17 13:04:47 -0600

    Added fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import '$lib/globals.css';
+</script>
+
+<style>
+    .navbar {
+        width: 100%
+    }
+
+    .nav-title {
+       font-family: 'Rubik', 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+       font-weight: 700;
+       font-size: 4rem;
+       margin: 0;
+    }
+
+    .container {
+        margin-left: 30%;
+        margin-right: 30%;
+    }
+</style>
+
+<div class="container">
+    <nav class="navbar">
+        <p class="nav-title">🐸 frog</p>
+    </nav>
+    <slot></slot>
+</div>
